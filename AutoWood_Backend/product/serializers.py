bf2ffbf066112e9dd9fd3be53b435d29c0e8125c
advdylan <immortalizetimes@gmail.com>
2024-04-23 18:06:19 +0200

    serializer/urls

--- AutoWood_Backend/product/serializers.py
+++ AutoWood_Backend/product/serializers.py
@@ -3,7 +3,54 @@ from rest_framework.reverse import reverse
 
 from .models import *
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class PaintsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paints
+        fields = '__all__'
+
+class WorktimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Worktime
+        fields = '__all__'
+
+class AccessorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Accessory
+        fields = '__all__'
+
+class WoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wood
+        fields = [
+            'name',
+            'density',
+            'price'
+        ]
+
+class ElementSerializer(serializers.ModelSerializer):
+
+    wood_type = WoodSerializer(read_only=True)
+    class Meta:
+        model = Element
+        fields = '__all__'
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = '__all__'
+
 class ProductSerializer(serializers.ModelSerializer): 
+    category = CategorySerializer(read_only=True)
+    paints = PaintsSerializer(read_only=True)
+    worktimes = WorktimeSerializer(many=True, read_only=True)
+    accessories = AccessorySerializer(many=True, read_only=True)
+    elements = ElementSerializer(many=True, read_only=True)
+    collection = CollectionSerializer(read_only=True)
 
     class Meta:
         model = Product
@@ -17,12 +64,3 @@ class ProductSerializer(serializers.ModelSerializer):
             'collection'
         ]
 
-
-class WoodSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Wood
-        fields = [
-            'name',
-            'density',
-            'price'
-        ]
