48229aca07945410bd61b775392d83e93b6eff42
advdylan <immortalizetimes@gmail.com>
2024-04-28 08:00:58 +0200

    vue

--- Client/autowood_frontend/src/App.vue
+++ Client/autowood_frontend/src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+ 
 </template>
 
 <style lang="scss">
@@ -16,16 +12,4 @@
 
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
