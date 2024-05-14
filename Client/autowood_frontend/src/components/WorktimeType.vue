b3188004504a97a9408ada7118beccad64483998
advdylan <immortalizetimes@gmail.com>
2024-05-14 16:53:59 +0200

    ok

--- Client/autowood_frontend/src/components/WorktimeType.vue
+++ Client/autowood_frontend/src/components/WorktimeType.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container">
     <div class="columns is-multiline">
-      <div class="column is-half" v-for="box in boxes" :key="box.text">
+      <div class="column is-full" v-for="box in boxes" :key="box.text">
         <label class="checkbox">
           <input type="checkbox" v-model="box.checked" />
           {{ box.text }}
         </label>
-        <input class="input" type="text" v-if="box.checked" v-model="box.input" />
+        <input  class="input" type="text" v-if="box.checked" v-model="box.input" />
         <div class="box" v-if="box.checked"> Stawka godzinowa:{{ box.value }}</div>
       </div>
     </div>
