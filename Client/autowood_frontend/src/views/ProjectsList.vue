b137d1f0b24d06453d9dcb8085bddc3562465cf1
advdylan <immortalizetimes@gmail.com>
2024-06-08 17:06:31 +0200

    searchable finish. Jade do Albanii elo

--- Client/autowood_frontend/src/views/ProjectsList.vue
+++ Client/autowood_frontend/src/views/ProjectsList.vue
@@ -1,16 +1,13 @@
 <template>
 
     <div class="projects-list-container">
-
         <b-table :data="data" :columns="columns"></b-table>
-      
     </div>
 
     
 </template>
   
 
-  
 <script setup>
 import { useProjectsListStore } from '@/store/projectslist'
 import { storeToRefs } from 'pinia'
