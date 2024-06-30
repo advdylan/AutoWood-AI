b55ee0dea3117d16176aae0c927e24dadecd915f
advdylan <immortalizetimes@gmail.com>
2024-06-30 15:31:13 +0200

    projektuje

--- Client/autowood_frontend/src/views/NewProjectDetail.vue
+++ Client/autowood_frontend/src/views/NewProjectDetail.vue
@@ -3,18 +3,13 @@
     <header class="card-header">
       <p class="card-header-title is-centered is-size-4">Wycena zamówienia "{{ detail_project.name}}"</p>
     </header>
-  <div class="columns">
-    <div class="column is-half">
-      
-      </div>
-   
-    <div class="column is-half">
+    <div class="card-content">
 
-  </div>
+      <NewProject></NewProject>
+
 </div>
 </div>
    
-<ElementsTable v-if="detail_project && detail_project.elements && detail_project.elements.length" :elements="detail_project.elements" />
    
     
 </template>
@@ -27,23 +22,27 @@
   </script>
   
   <script setup>
+  import { useNewProjectStoreBeta } from '@/store/newproject'
   import { useProjectsListStore } from '@/store/projectslist'
   import { storeToRefs } from 'pinia'
   import { onMounted } from 'vue'
   import { useRoute } from 'vue-router'
   import ElementsTable from '@/components/ElementsTable'
+  import NewProject from './NewProject.vue'
 
   const route = useRoute()
   const id = route.params.id
   
   const ProjectsListStore = useProjectsListStore()
+  const elementStore = useNewProjectStoreBeta()
+
+
   const { loadDetailProject, } = ProjectsListStore
   const {projectlist, detail_project} = storeToRefs(ProjectsListStore)
   
   
-  function displayElements() {
-    console.log(detail_project.value)
-  } 
+  
+
   
   loadDetailProject(id)
 
