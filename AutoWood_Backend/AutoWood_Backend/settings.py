2ba698f6a9c4fdc2e2185bb5056bf497ce891b0f
Dylan <immortalizetimes@gmail.com>
2024-11-06 21:17:44 +0100

    new requirements/setting with startup.sh and secret key

--- AutoWood_Backend/AutoWood_Backend/settings.py
+++ AutoWood_Backend/AutoWood_Backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -107,6 +108,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+env = Env()
+env.read_env()
+SECRET_KEY = env.str("SECRET_KEY", "default-secret-key-if-none")
+
 
 
 # Password validation
--- AutoWood_Backend/startup.sh
+++ AutoWood_Backend/startup.sh
@@ -2,4 +2,4 @@
 python manage.py migrate
 sqlite3 db.sqlite3 'PRAGMA journal_mode=WAL;'
 sqlite3 db.sqlite3 'PRAGMA synchronous=1;'
-gunicorn --bind :8000 --workers 2 wsgi
+gunicorn --bind :8000 --workers 2 AutoWood_Backend.wsgi
