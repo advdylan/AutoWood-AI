e3eb8ed1651dc88b4234e170ed583c54805282b5
Dylan <immortalizetimes@gmail.com>
2024-11-06 16:47:56 +0100

    cors settings

--- AutoWood_Backend/AutoWood_Backend/settings.py
+++ AutoWood_Backend/AutoWood_Backend/settings.py
@@ -56,6 +56,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "https://auto-wood-git-master-jakubs-projects-901e278c.vercel.app",
     "https://auto-wood-jakubs-projects-901e278c.vercel.app",
+    "https://autowood.fly.dev/",
+    "autowood.fly.dev/"
 
 ]
 
