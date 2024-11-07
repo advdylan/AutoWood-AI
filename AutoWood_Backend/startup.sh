5e6c57410b679b6259ed6c40deaa0f5688a8bf95
Dylan <immortalizetimes@gmail.com>
2024-11-07 15:48:44 +0100

    moving to postregsql

--- AutoWood_Backend/startup.sh
+++ AutoWood_Backend/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python manage.py migrate
 #python manage.py loaddata initial_json.json
-sqlite3 db.sqlite3 'PRAGMA journal_mode=WAL;'
-sqlite3 db.sqlite3 'PRAGMA synchronous=1;'
+sqlite3 db.sqlite 'PRAGMA journal_mode=WAL;'
+sqlite3 db.sqlite 'PRAGMA synchronous=1;'
 gunicorn --bind :8000 --workers 2 AutoWood_Backend.wsgi:application
