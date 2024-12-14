8bbd2d2cbbee1c27695b4b347b8419b8a9689265
Dylan <immortalizetimes@gmail.com>
2024-12-14 17:16:03 +0100

    burntout

--- Client/autowood_frontend/src/views/Home.vue
+++ Client/autowood_frontend/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     
-    <section class="hero is-small is-primary mb-6" style="background-color: rgb(242, 248, 254);">
+    <section class="hero is-small is-primary mb-6" style="background-color: rgb(137, 168, 178);">
         <div class="hero-body">
-          <p class="title" style="color: rgb(120, 157, 188);">{{$t("good_morning")}} 
+          <p class="title">{{$t("good_morning")}} 
           </p>
           
         </div>
@@ -12,13 +12,13 @@
       <div class="columns">
         <div class="column is-half is-offset-one-quarter">
           <div class="card">
-            <header  class="card-header" style="background-color: rgb(242, 248, 254) ;">
-              <p class="card-header-title is-centered is-size-4" style="color: rgb(120, 157, 188);">{{$t("shortcuts")}}  &nbsp;<i class="fa-solid fa-arrow-up-right-from-square"></i></p>
+            <header  class="card-header" style="background-color: rgb(137, 168, 178) ;">
+              <p class="card-header-title is-centered is-size-4" style="color: white" >{{$t("shortcuts")}}  &nbsp;<i class="fa-solid fa-arrow-up-right-from-square"></i></p>
               
             </header>
 
 
-            <div class="card-content" style="background-color: rgb(242, 248, 254) ;">
+            <div class="card-content" style="background-color: rgb(137, 168, 178) ;">
               <div class="content">
                 <div class="columns">
                   <div class="column is-half">
