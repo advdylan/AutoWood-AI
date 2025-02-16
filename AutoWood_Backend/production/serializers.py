1f368304cd4f5b99e287a75558e4493119436f74
Dylan <immortalizetimes@gmail.com>
2025-02-16 19:09:29 +0100

    production serializer

--- AutoWood_Backend/production/serializers.py
+++ AutoWood_Backend/production/serializers.py
@@ -4,21 +4,22 @@ from rest_framework.reverse import reverse
 from .models import *
 from warehouse.models import *
 from production.models import *
-from product.serializers import NewProjectSerializer, WorktimeTypeSerializer, AccessoryTypeSerializer
+from product.serializers import NewProjectSerializer, WorktimeTypeSerializer, AccessoryTypeSerializer, WoodSerializer, ElementSerializer, PaintsSerializer, CategorySerializer, CollectionSerializer
 
 
 
 
 class WorktimeSerializer(serializers.ModelSerializer):
-    worktime = WorktimeTypeSerializer(read_only=True)  # Serialize the related Worktimetype model
-    #worktime_id = serializers.PrimaryKeyRelatedField(queryset=Worktimetype.objects.all(), source='worktime', write_only=True)
 
+    worktime = WorktimeTypeSerializer(read_only=True) 
     name = WorktimeTypeSerializer(read_only = True, many = True)
+
     class Meta:
         model = CatalogWorktime
         fields = '__all__'
 
 class AccesoryTypeSerializer(serializers.ModelSerializer):
+
     accesory = AccessoryTypeSerializer(read_only=True)
     name = AccessoryTypeSerializer(read_only = True, many = True)
 
@@ -26,11 +27,17 @@ class AccesoryTypeSerializer(serializers.ModelSerializer):
         model = CatalogAccessoryDetail
         fields = '__all__'
 
+class CatalogElementSerializer(serializers.ModelSerializer):
+    element = ElementSerializer(read_only = True)
+    name = ElementSerializer(read_only = True, many = True)
+
+    class Meta:
+        model = CatalogElement
+        fields = '__all__'
 
 
 class ProductionStagesSerializer(serializers.ModelSerializer):
 
-    
     class Meta:
         model = ProductionStage
         fields = '__all__'
@@ -38,8 +45,14 @@ class ProductionStagesSerializer(serializers.ModelSerializer):
 
 class CatalogProductSerializer(serializers.ModelSerializer):
 
-    worktimes = WorktimeSerializer(many=True, read_only=True, source='catalog_worktime')
-    accessories = AccesoryTypeSerializer(many=True, read_only=True, source='catalog_accessories')
+    worktimes = WorktimeSerializer(many=True, read_only=True, source = 'catalog_worktime')
+    accessories = AccesoryTypeSerializer(many=True, read_only=True, source = 'catalog_accessories')
+    elements = CatalogElementSerializer(many=True,read_only = True, source = 'catalog_elements')
+    wood = WoodSerializer( read_only=True)
+    paints = PaintsSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
+    collection = CollectionSerializer(read_only=True)
+
     
 
     class Meta:
@@ -49,7 +62,12 @@ class CatalogProductSerializer(serializers.ModelSerializer):
             'name',
             'category',
             'worktimes',
-            'accessories'
+            'accessories',
+            'elements',
+            'wood',
+            'paints',
+            'category',
+            'collection',
             'elements_margin',
             'accesories_margin',
             'additional_margin',
@@ -98,45 +116,17 @@ class GenericRelatedField(serializers.Field):
 class ProductionSerializer(serializers.ModelSerializer):
 
     stages = ProductionStagesSerializer(many=True, read_only=True, source='production_stages')    
-    order = GenericRelatedField
+    order = GenericRelatedField()
 
     class Meta:
         model = Production
         fields = [
             "stages",
+            "order",
+            "status",
+            "date_ordered",
+            "date_of_delivery"
          
         ]
 
 
-
-class GenericRelatedField(serializers.Field):
-
-    def to_representation(self, value):
-        if isinstance(value, CatalogProduct):
-            return CatalogProductSerializer(value).data
-        elif isinstance(value, NewProject):
-            return NewProjectSerializer(value).data
-        
-
-    def to_internal_value(self, data):
-        model = data.get("type")
-        object_id = data.get("id")
-
-        if not model or not object_id:
-            raise serializers.ValidationError("type and id required")
-        
-        model_map = {
-            "catalog_product": CatalogProduct,
-            "new_project": NewProject
-        }
-
-        ModelClass = model_map.get(model.lower())
-
-        if not ModelClass:
-            raise serializers.ValidationError(f"Invalid type {model} Type 'catalog_product' or 'new_project'")
-
-
-        try:
-            return ModelClass.objects.get(id=object_id)
-        except ModelClass.DoesNotExist:
-            raise serializers.ValidationError(f"No {model} found with id {object_id}")
