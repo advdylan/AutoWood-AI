42dc06b21d3681fb7cc62af7999261ccc8648712
Dylan <immortalizetimes@gmail.com>
2025-02-18 20:19:48 +0100

    production

--- Client/autowood_frontend/src/store/production.js
+++ Client/autowood_frontend/src/store/production.js
@@ -12,5 +12,17 @@ export const productionStore = defineStore('productionStore', {
   getters: {
   }, 
   actions: {
+
+    async getProductionList() {
+
+      await axios
+      .get(`/api/v1/production/production-list`)
+      .then(response =>{
+        console.log(response)
+        this.productionList = response.data
+      })
+      .catch(error => {console.log(error)})
+
+    }
   }
 })
--- Client/autowood_frontend/src/views/Production.vue
+++ Client/autowood_frontend/src/views/Production.vue
@@ -9,6 +9,18 @@
                 <div class="column is-full">
                     <div class="label has-text-centered is-size-5"> {{ $t('production') }}</div>
                     <div class="box">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Order number</th>
+                                    <th>Order name</th>
+                                    <th>Category</th>
+                                </tr>
+
+                            </thead>
+                        </table>
+
+
                         
                         
                     </div>
@@ -27,10 +39,40 @@
 
 <script setup>
 import axios from 'axios';
+import { productionStore } from '@/store/production';
+import { ref, onMounted, computed } from 'vue';
+import { storeToRefs } from 'pinia';
+
+//store data 
+const store = productionStore()
+const {getProductionList} = store
+const {productionList} = storeToRefs(store)
+
+//const variables
+
+//computed values
+const processedProductionList = ref([])
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+onMounted(() => {
+    getProductionList()
+})
 </script>
 
 <style lang="css" scoped>
